92abcd5a92082a6c3aae3ba41df4d819f3f29945
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-01-25 11:29:05 -0500

    made state variables and initial fetch request

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stockList, setStockList] = useState([])
+  const [porfolio, setPortfolio] = useState([])
+
+  useState(() => {
+    fetch('http://localhost:3001/stocks')
+    .then((r) => r.json())
+    .then((data) => console.log(data))
+    // .then((data) => setStockList(data))
+  }, [])
+  
   return (
     <div>
       <SearchBar />
